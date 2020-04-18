85fe1caf480269ade19a681468ede9a70a3f4cc4
Bloon <BloonZhang@gmail.com>
2020-04-18 03:28:56 -0500

    drag and drop code

---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+
+    // public variables
+    public Vector2 pointToSnapTo;
+    public Transform parentToSnapTo;
+
+    // Helper variables
+    private Vector2 dragPositionOffset;
+    //private Vector2 initialPosition;
+    //private Transform initialParent;
+    //private DropZone initialDropZone;
+
+
+    public void OnBeginDrag(PointerEventData eventData) {
+        //Debug.Log("OnBeginDrag");
+        // Position to snap back to if drop fails
+        pointToSnapTo = this.transform.position;
+        // DropZone that token currently resides in
+        //initialDropZone = this.transform.parent.GetComponent<DropZone>();
+        // Get offsett between transform and mouse position
+        dragPositionOffset = (Vector2)eventData.position - (Vector2)this.transform.position;
+        // Turn off raycasts
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+        // Get initial parent and move upwards in heirarchy
+        parentToSnapTo = this.transform.parent;
+        this.transform.SetParent(this.transform.parent.parent);
+    }
+
+    public void OnDrag(PointerEventData eventData) {
+        //Debug.Log("OnDrag");
+        Vector2 positionToMoveTo = eventData.position - dragPositionOffset;
+        this.transform.position = positionToMoveTo;
+    }
+
+    public void OnEndDrag(PointerEventData eventData) {
+        Debug.Log("OnEndDrag");
+        // Turn raycasts back on
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+        //Debug.Log(parentToSnapTo.GetComponent<DropZone>().ContainsPoint(this.transform.position));
+        Debug.Log("this tranform position is: " + this.transform.position);
+        parentToSnapTo.GetComponent<DropZone>().DebugMe();
+        this.transform.SetParent(parentToSnapTo);
+        this.transform.position = pointToSnapTo;
+    }
+}
---
+++ Assets/Scripts/DropZone.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler
+{
+    // Info about bounds
+    private float leftEdge;
+    private float rightEdge;
+    private float topEdge;
+    private float bottomEdge;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rect myRect = GetComponent<RectTransform>().rect;
+        leftEdge = myRect.xMin;
+        rightEdge = myRect.xMax;
+        topEdge = myRect.yMin;
+        bottomEdge = myRect.yMax;
+        // todo: screen point instead of relative point?
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log ("dropped into " + gameObject.name);
+        // Get the draggable that was dropped into it
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        if (d != null) {
+            d.parentToSnapTo = this.transform;
+            d.pointToSnapTo = d.transform.position;
+        }
+    }
+
+    // Method for seeing if a point is contained within this drop zone
+    public bool ContainsPoint(Vector2 position)
+    {
+         return (position.x > leftEdge && position.x < rightEdge && position.y > topEdge && position.y < bottomEdge);
+    }
+
+    public void DebugMe()
+    {
+        Debug.Log("left edge: " + leftEdge);
+        Debug.Log("right edge: " + rightEdge);
+        Debug.Log("top edge: " + topEdge);
+        Debug.Log("bot edge: " + bottomEdge);
+    }
+}
